dca39f0d952847c3a1c9efda4883ede0bb92e3ec
orkhanAbdulla <Orkhanaa@code.edu.az>
2022-08-17 19:08:14 +0400

    AppDbContext Class added

---
+++ ExcelUploadReadDataSave.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelUploadReadDataSave.Persistence.Context
+{
+    public class AppDbContext
+    {
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+            public DbSet<Product> Products { get; set; }
+            public DbSet<Order> Orders { get; set; }
+            public DbSet<Customer> Customers { get; set; }
+        }
+    }
+}
