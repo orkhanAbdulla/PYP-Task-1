1c8df07f6e4ebe200c1f1dc12a369349a2006f67
orkhanAbdulla <Orkhanaa@code.edu.az>
2022-08-18 01:24:14 +0400

    Repository Interfaces added

---
+++ ExcelUploadReadDataSave.Application/Repositories/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using ExcelUploadReadDataSave.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelUploadReadDataSave.Application.Repositories
+{
+    public interface IRepository<T>  where T:BaseEntity
+    {
+        DbSet<T> Table { get; }
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
+        Task<T> GetById(int id);
+        Task<bool> AddAsync(T model);
+        Task<bool> AddRangeAsync(List<T> datas);
+        bool Remove(T model);
+        Task<bool> RemoveAsync(int id);
+        bool RemoveRange(List<T> datas);
+        bool Update(T model);
+        Task<int> SaveAsync();
+    }
+}
