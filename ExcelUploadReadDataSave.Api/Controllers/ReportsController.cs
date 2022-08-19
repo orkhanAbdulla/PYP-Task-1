ea08224d060467646b3f53031a253c0f4b97fa02
orkhanAbdulla <Orkhanaa@code.edu.az>
2022-08-19 05:34:23 +0400

    Configure ExceptionHandlerService and UploadData Action Is done

---
+++ ExcelUploadReadDataSave.Api/Controllers/ReportsController.cs
@@ -0,0 +1,25 @@
+﻿using ExcelUploadReadDataSave.Application.DTOs.ReportDto;
+using ExcelUploadReadDataSave.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcelUploadReadDataSave.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IReportService _reportService;
+
+        public ReportsController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+        [HttpPost("")]
+        public async Task<IActionResult> UploadData([FromForm]ReportUploadFileDto reportUploadFileDto)
+        {
+           await _reportService.UploadDataFile(reportUploadFileDto);
+           return Ok();
+        }
+    }
+}
---
+++ ExcelUploadReadDataSave.Api/ServiceExtentions/ExceptionHandlerService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Mime;
+using ExcelUploadReadDataSave.Application.CustomExceptions;
+
+
+namespace ExcelUploadReadDataSave.Api.ServiceExtentions
+{
+    public static class ExceptionHandlerService
+    {
+        public static void AddExceptionHandlerExtention(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(error =>
+            {
+                error.Run(async context =>
+                {
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var code = 500;
+                    string message = "Inter Server Error. Please Try Again Later!";
+
+                    if (contextFeature != null)
+                    {
+                        message = contextFeature.Error.Message;
+                         if (contextFeature.Error is FileFormatException)
+                        {
+                            code = 400;
+                        }
+                        //else if (contextFeature.Error is ItemNotFoundException)
+                        //{
+                        //    code = 404;
+                        //}
+                        //else if (contextFeature.Error is RecordAlreadyExistException)
+                        //{
+                        //    code = 409;
+                        //}
+                    
+                        //else if (contextFeature.Error is ItemNotFoundException)
+                        //{
+                        //    code = 400;
+                        //}
+                    }
+                    context.Response.StatusCode = code;
+                    var JsonStr = JsonConvert.SerializeObject(new
+                    {
+                        code = code,
+                        message = message
+                    });
+                    await context.Response.WriteAsync(JsonStr);
+                });
+            });
+
+        }
+    }
+}
