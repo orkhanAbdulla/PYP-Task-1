959705c636b6384118362fbc8448de5a605e3d55
orkhanAbdulla <Orkhanaa@code.edu.az>
2022-08-21 02:20:23 +0400

    ReportService Changed

--- ExcelUploadReadDataSave.Persistence/Services/ReportService.cs
+++ ExcelUploadReadDataSave.Persistence/Services/ReportService.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static ExcelUploadReadDataSave.Application.Utilis.Enums;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace ExcelUploadReadDataSave.Persistence.Services
 {
@@ -21,7 +22,8 @@ namespace ExcelUploadReadDataSave.Persistence.Services
         private readonly IFileManager _fileManager;
         private readonly IMapper _mapper;
 
-        public ReportService(IReportRepository reportRepository, IFileManager fileManager,IMapper mapper)
+
+        public ReportService(IReportRepository reportRepository, IFileManager fileManager, IMapper mapper)
         {
             _reportRepository = reportRepository;
             _fileManager = fileManager;
@@ -30,58 +32,78 @@ namespace ExcelUploadReadDataSave.Persistence.Services
 
         public async Task UploadDataFile(ReportUploadFileDto FileDto)
         {
-            if (!_fileManager.IsValidType(FileDto,".xlxs"))
-                   throw new FileFormatException("File extention must be .xlsx or .xls");
+            if (!_fileManager.IsValidType(FileDto, ".xlxs"))
+                throw new System.IO.FileFormatException("File extention must be .xlsx or .xls");
             if (!_fileManager.IsValidSize(FileDto, 5120))
-                   throw new FileFormatException("File must be less than 5 kb");
+                throw new System.IO.FileFormatException("File must be less than 5 kb");
 
             List<Report> reports = _fileManager.ExcelDataReader(_fileManager.Save(FileDto));
-                await _reportRepository.AddRangeAsync(reports);
-                await _reportRepository.SaveAsync();
+            await _reportRepository.AddRangeAsync(reports);
+            await _reportRepository.SaveAsync();
         }
-        public async Task GetReport(SendReportDto sendReportDto,int type)
+        public async Task<ReportResultDtos> GetReport(SendReportDto sendReportDto, int type)
         {
             int result = DateTime.Compare(sendReportDto.StartDate, sendReportDto.EndDate);
-            if (result>0 ||result==0)
+            if (result > 0 || result == 0)
                 throw new DateIntervalException("Start Date must be more End date");
             if (!Enum.IsDefined((ReportType)type))
-               throw new NotReportTypeException("Please enter correct report type!");
+                throw new NotReportTypeException("Please enter correct report type!");
             ReportType reportType = (ReportType)type;
+
+            ReportResultDtos reportResultDto = new ReportResultDtos();
+
+
+            IQueryable<Report> SegmentDb = _reportRepository.GetWhere(x => x.Data >= sendReportDto.StartDate && x.Data <= sendReportDto.EndDate);
+
 
             switch (reportType)
             {
                 case ReportType.SegmentSales:
 
-                    IQueryable<Report> SegmentDb = _reportRepository.GetWhere(x =>x.Data >= sendReportDto.StartDate && x.Data <= sendReportDto.EndDate);
-                    List<SegmentGetDto> segmentGetDtos = _mapper.Map<List<SegmentGetDto>>(SegmentDb);
-
-                    Dictionary<string, SegmentGetDto> segmentTotal = new();
-
-                    foreach (SegmentGetDto segmentGetDto in segmentGetDtos)
-                    {
-                        if (!segmentTotal.ContainsKey(segmentGetDto.Segment))
-                        {
-                            segmentTotal.Add(segmentGetDto.Segment, segmentGetDto);
-                            continue;
-                        }
-                        segmentTotal[segmentGetDto.Segment].UnitsSold += segmentGetDto.UnitsSold;
-                        segmentTotal[segmentGetDto.Segment].GrossSales += segmentGetDto.GrossSales;
-                        segmentTotal[segmentGetDto.Segment].Discounts += segmentGetDto.Discounts;
-                        segmentTotal[segmentGetDto.Segment].Profit += segmentGetDto.Profit;
-                    }
-                    Console.WriteLine(segmentTotal);
-
+                    reportResultDto.reportGetDtos = SegmentDb.GroupBy(x => x.Segment)
+                          .Select(g => new ReportGetDto
+                          {
+                              Type = g.Key,
+                              UnitsSold = g.Sum(x => x.Discounts),
+                              GrossSales = g.Sum(x => x.GrossSales),
+                              Discounts = g.Sum(x => x.Discounts),
+                              Profit = g.Sum(x => x.Profit),
+                          }).ToList();
                     break;
                 case ReportType.CountrySales:
+
+                    reportResultDto.reportGetDtos = SegmentDb.GroupBy(x => x.Country)
+                         .Select(g => new ReportGetDto
+                         {
+                             Type = g.Key,
+                             UnitsSold = g.Sum(x => x.Discounts),
+                             GrossSales = g.Sum(x => x.GrossSales),
+                             Discounts = g.Sum(x => x.Discounts),
+                             Profit = g.Sum(x => x.Profit),
+                         }).ToList();
                     break;
                 case ReportType.ProductSales:
+
+                    reportResultDto.reportGetDtos = SegmentDb.GroupBy(x => x.Product)
+                .Select(g => new ReportGetDto
+                {
+                    Type = g.Key,
+                    UnitsSold = g.Sum(x => x.Discounts),
+                    GrossSales = g.Sum(x => x.GrossSales),
+                    Discounts = g.Sum(x => x.Discounts),
+                    Profit = g.Sum(x => x.Profit),
+                }).ToList();
                     break;
                 case ReportType.ProductDiscount:
+                    reportResultDto.reportProductDiscountDtos = SegmentDb.GroupBy(x => x.Product)
+                .Select(g => new ReportProductDiscountDto
+                {
+                    Type = g.Key,
+                    Percent = g.Sum(x => x.Discounts) / g.Sum(x => x.GrossSales) * 100
+                }).ToList();
                     break;
             }
-        
+            return reportResultDto;
         }
-
-
     }
 }
